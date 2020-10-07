19c1b0217bf68876604908ac1e38e68c0c440838
Ritheesh <ritheesh.4@gmail.com>
2020-10-07 10:00:01 +0530

    directory component added

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,41 +1,12 @@
 import React from "react";
 
-import './hompepage.styles.scss';
+import Directory from "../../components/directory/directory.component";
+
+import "./hompepage.styles.scss";
 
 const HomePage = () => (
   <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="sub-title">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">JACKETS</h1>
-          <span className="sub-title">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">SNEAKERS</h1>
-          <span className="sub-title">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">MENS</h1>
-          <span className="sub-title">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">WOMENS</h1>
-          <span className="sub-title">SHOP NOW</span>
-        </div>
-      </div>    
-    </div>
+    <Directory />
   </div>
 );
 
