81fda4357cdaceaf587ef90c40ba2235cf19043c
Ritheesh <ritheesh.4@gmail.com>
2020-10-09 16:57:30 +0530

    Sing in page basic files and component for sing in added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component.jsx';
+import Header from './components/header/header.component';
+import SignInANdSignUpPage from './pages/sign-in-and-sing-up/sign-in-and-sing-up.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signIn' component={SignInANdSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sing-up/sign-in-and-sing-up.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./sign-in-and-sign-up.styles.scss";
+
+const SignInAndSignUpPage = () => (
+  <div className="sign-in-and-sign-up">SING IN</div>
+);
+
+export default SignInAndSignUpPage;
