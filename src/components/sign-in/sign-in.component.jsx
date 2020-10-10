9581e1fb093ad74ab603a1c315dee3b754d8dffb
Ritheesh <ritheesh.4@gmail.com>
2020-10-10 10:25:23 +0530

    basic form input added

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,16 +1,60 @@
-import React from 'react';
+import React from "react";
 
-import './sign-in.styles.scss';
+import FormInput from "../form-input/form-input.component";
+
+import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
-    constructor(props){
-        super();
+  constructor(props) {
+    super();
 
-        this.state = {
-            email:'',
-            password:''
-        }
-        
-    }
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({ email: "", passowrd: "" });
+  };
+
+  handleChange = (event) => {
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="email"
+            name="email"
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label="email"
+            required
+          />
+          <label>Email</label>
+          <FormInput
+            type="password"
+            name="password"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            label="password"
+            required
+          />
+          <label>Password</label>
+
+          <input type="submit" value="Submit form" />
+        </form>
+      </div>
+    );
+  }
 }
 
+export default SignIn;
