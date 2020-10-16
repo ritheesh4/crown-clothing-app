120b2fc1c37b4e97f9566fa59e48c012e757e416
Ritheesh <ritheesh.4@gmail.com>
2020-10-16 09:19:01 +0530

    add to cart buttons on the  shop page implemented

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import CustomButton from "../custom-button/custom-button.component";
 
-import './collection-item.styles.scss';
+import "./collection-item.styles.scss";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className="collection-item">
@@ -14,6 +15,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className="name">{name}</span>
       <span className="price">{price}</span>
     </div>
+    <CustomButton inverte>Add to cart</CustomButton>
   </div>
 );
 
